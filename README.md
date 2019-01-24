2b00149c2c8ceee980685133158d4a990029728b
Hugh Gao <hughgao01@gmail.com>
2019-01-24 20:59:22 +0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Everlasting
+
+# 创建用户及数据库
+
+# 参考项目
+* https://gitee.com/stylefeng/guns
---
+++ src/main/kotlin/com/klniu/everlasting/EverlastingApplication.kt
@@ -0,0 +1,12 @@
+package com.klniu.everlasting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EverlastingApplication
+
+fun main(args: Array<String>) {
+    runApplication<EverlastingApplication>(*args)
+}
+
